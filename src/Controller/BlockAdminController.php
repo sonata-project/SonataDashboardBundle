9a7441154a9828331eb8903c623ac6decbb43252
Sullivan SENECHAL <soullivaneuh@gmail.com>
2020-05-28 04:09:02 +0200

    Applied fixes from FlintCI

--- src/Controller/BlockAdminController.php
+++ src/Controller/BlockAdminController.php
@@ -119,7 +119,7 @@ final class BlockAdminController extends Controller
     {
         $blockId = $request->get('block_id');
         $parentId = $request->get('parent_id');
-        if (null === $blockId or null === $parentId) {
+        if (null === $blockId || null === $parentId) {
             throw new HttpException(400, 'wrong parameters');
         }
 
