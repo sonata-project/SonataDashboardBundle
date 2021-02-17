569064cdff66a3884ddfd85ee2193d4d90ab9b4c
Sonata CI <thomas+ci@sonata-project.org>
2021-02-17 15:36:52 +0000

    DevKit updates for master branch (#294)
    * DevKit updates
    
    * Applied fixes from FlintCI (#295)
    
    Co-authored-by: Sullivan SENECHAL <soullivaneuh@gmail.com>

--- tests/Fixtures/Entity/FooGetNameNull.php
+++ tests/Fixtures/Entity/FooGetNameNull.php
@@ -1,16 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Sonata\DashboardBundle\Tests\Fixtures\Entity;
 
 final class FooGetNameNull
 {
-    public function getName()
-    {
-        return;
-    }
-
     public function __toString()
     {
         return 'GetNameNull';
+    }
+
+    public function getName()
+    {
     }
 }
